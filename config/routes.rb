e2559ebd0331d340113b79d595e9fb9740742f19
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-17 20:30:12 -0600

    set up user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   resources :jobs
   root to: 'home#index'
-  devise_for :users
+  devise_for :users,
+              path: '',
+              path_names: {
+                sign_in: 'login',
+                sign_out: 'logout',
+                registration: 'signup'
+              },
+              controllers: {
+                sessions: 'sessions',
+                registrations: 'registrations'
+              }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

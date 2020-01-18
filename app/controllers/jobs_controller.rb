988aefeba82e127734dbb6e957b48a563fa26a2c
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-18 00:42:18 -0600

    set up job controller CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,48 +2,49 @@ class JobsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @jobs = current_user.jobs
+    jobs = current_user.jobs
 
-    render json: @jobs, status: 200
+    render json: jobs, status: 200
   end
 
   def new
-    @job = Job.new
+    job = Job.new
   end
 
   def create
-    @job = current_user.jobs.build(job_params)
+    job = Job.new(job_params)
+    job.user = current_user
+    job.save
+    render_resource(job)
 
-    if @job.save
-      render json: @jobs, status: 200
-    else
-      render json: {status: "error", code: 3000, message: "Your Job Application did not save!"}
-  end
 end
 
   def show
-    @job = Job.find(params[:id])
-    authorize_user_resource(jobs)
+    job = Job.find(params[:id])
+    authorize_user_resource(job)
 
-    render json: @job, status: 200
+    render_resource(job)
   end
 
   def edit
-    @job = Job.find_by_id(params[:id])
+    job = Job.find_by_id(params[:id])
   end
 
   def update
-    @job = Job.find_by_id(params[:id])
-    @job.update(job_params)
+    job = Job.find(params[:id])
+    authorize_user_resource(job)
+    job.update(job_params)
+    render_resource(job)
 
-    render json: @jobs, status: 200
   end
 
   def destroy
-    @job = Job.find_by_id(params[:id])
-    @job.destroy
+    job = Job.find(params[:id])
+    authorize_user_resource(job)
+    job.destroy
+    render_resource(job)
 
-    # render json: {jobId :@job.id, message: "Has been deleted!"}
+    # render json: {jobId :job.id, message: "Has been deleted!"}
   end
 
   private
@@ -51,6 +52,6 @@ end
     params.require(:job).permit(:company_name, :contact_name, :email, :fax,
       :phone1, :phone2, :has_applied, :application_date, :website, :application_link,
       :has_phone_interview, :phone_interview_date, :phone_interview_notes, :has_in_person,
-      :in_person_interview, :in_person_notes, :company_notes, :salary, :user_id, address:{})
+      :in_person_interview, :in_person_notes, :company_notes, :salary, address:{})
   end
 end

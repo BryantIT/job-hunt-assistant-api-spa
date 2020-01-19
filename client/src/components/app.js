c029dd070377d0fdc5f6ed92c670580bb4a695df
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-19 06:16:55 -0600

    setup binding

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -3,11 +3,16 @@ class App {
   constructor() {
 
     this.initBindingsAndEventListeners()
+    this.renderPage(new SignupPage(this.pageContainer))
   }
 
   initBindingsAndEventListeners() {
     this.container = document.querySelector('#app-container')
     this.navbarContainer = document.querySelector('#navbar-container')
     this.pageContainer = document.querySelector('#page-container')
+  }
+
+  renderPage(page) {
+    page.render()
   }
 }

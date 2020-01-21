5af4e472cdc714754d3e5a9e61eb4bff277a9ab8
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-21 09:39:39 -0600

    added baseadapter to App constructor

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,7 +1,7 @@
 class App{
 
   constructor(){
-
+    this.adapter = new BaseAdapter('http://localhost:3000')
     this.initBindingsAndEventListeners()
     this.renderPage(new SignupPage(this.pageContainer))
   }

95ac322407546987deff15cd6ae0bd79afdc757d
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-21 09:15:57 -0600

    made form inputs required

--- client/src/pageManagers/signup.js
+++ client/src/pageManagers/signup.js
@@ -12,20 +12,15 @@ class SignupPage {
             <form method="post" action="#">
               <div class="row uniform">
                 <div class="6u 12u$(xsmall)">
-                  <input type="text" name="name" id="name" value="" placeholder="Name" />
+                  <input type="text" name="name" id="name" value="" placeholder="Name" required >
                 </div>
                 <div class="6u$ 12u$(xsmall)">
-                  <input type="email" name="email" id="email" value="" placeholder="Email" />
+                  <input type="email" name="email" id="email" value="" placeholder="Email" required >
                 </div>
                 <div class="6u 12u$(xsmall)">
-                  <input type="password" name="password" id="password" value="" placeholder="Password" />
+                  <input type="password" name="password" id="password" value="" placeholder="Password" required>
                 </div>
-                <div class="6u 12u$(xsmall)">
-                  <input type="password" name="confirm-password" id="password" value="" placeholder="Confirm Password" />
-                </div>
-
-
-
+                
                 <div class="12u$">
                   <ul class="actions">
                     <li><input type="submit" value="Sign In" /></li>

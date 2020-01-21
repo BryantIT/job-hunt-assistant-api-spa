997c8ed8d889788e120c81bd922ccb8d914940cb
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-21 09:30:07 -0600

    set up baseAdapter

---
+++ client/src/adapters/baseAdapter.js
@@ -0,0 +1,19 @@
+class BaseAdapter{
+
+  constructor(baseURL){
+    this.baseURL = baseURL
+    this.token = null
+  }
+
+  get headers(){
+    const baseHeaders = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+
+    if(this.token){
+      baseHeaders = { ...baseHeaders, 'Authorization': `Bearer ${this.token}`}
+    }
+    return baseHeaders
+  }
+}

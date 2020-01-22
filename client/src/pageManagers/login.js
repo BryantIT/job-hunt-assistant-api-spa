61db59348a7c76a5ab3f8c1d62eb039a0f881e87
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-21 23:41:52 -0600

    login and signup renders an auth token works

--- client/src/pageManagers/login.js
+++ client/src/pageManagers/login.js
@@ -1,6 +1,49 @@
 class LoginPage extends PageManager{
 
+  constructor(container, adapter){
+    super(container)
+    this.adapter = new LoginAdapter(adapter)
+  }
+
   initBindingsAndEventListeners(){
-    return null
+    this.form = this.container.querySelector('form#login-form')
+    this.form.addEventListener('submit', this.handleSubmit.bind(this))
+  }
+
+  handleSubmit(e){
+    e.preventDefault()
+    const [email, password] = Array.from(e.target.querySelectorAll('input')).map(i => i.value)
+    const params = {
+      user: {email, password}
+    }
+    this.adapter.login(params)
+  }
+
+  get staticHTML() {
+    return (`
+      <section id="one" class="wrapper post bg-img" data-bg="banner2.jpg" style="background-image: url(assets/images/banner2.jpg);">
+        <div class="inner current">
+        <h2>Please Login</h2>
+          <article class="box">
+            <form id="login-form">
+              <div class="row uniform">
+                <div class="6u 12u$(xsmall)">
+                  <input type="email" name="email" id="email" value="" placeholder="Email" required >
+                </div>
+                <div class="6u$ 12u$(xsmall)">
+                  <input type="password" name="password" id="password" value="" placeholder="Password" required>
+                </div>
+
+                <div class="12u$">
+                  <ul class="actions">
+                    <li><button type="submit" class="button special fit">Login</button></li>
+                  </ul>
+                </div>
+              </div>
+            </form>
+          </article>
+        </div>
+      </section>
+    `)
   }
 }
--- client/src/pageManagers/signup.js
+++ client/src/pageManagers/signup.js
@@ -1,5 +1,10 @@
 class SignupPage extends PageManager{
 
+  constructor(container, adapter){
+    super(container)
+    this.adapter = new SignupAdapter(adapter)
+  }
+
   initBindingsAndEventListeners(){
     this.form = this.container.querySelector('#signup-form')
 
@@ -20,17 +25,27 @@ class SignupPage extends PageManager{
 
   get staticHTML() {
     return (`
-      <section id="one" class="wrapper post bg-img" data-bg="banner2.jpg">
-        <div class="inner">
-        <h2>Signup or Sign In</h2>
+      <section id="one" class="wrapper post bg-img" data-bg="banner2.jpg" style="background-image: url(assets/images/banner2.jpg);">
+        <div class="inner current">
+          <header>
+            <h3>Welcome to your job hunt assistant</h3>
+            <p>
+              This site is designed to assist you as you begin your job hunt.
+              Here you will be able to keep track of what jobs you are interested in applying for
+              and have applied for. Prompts will encourage you to research the company you are sending
+              your resume to. You will also be able to create notes of your experience with each company
+              that you can come back to for review.
+            </p>
+            <h4>Good luck on your hunt!</h4>
+          </header>
           <article class="box">
             <form id="signup-form">
               <div class="row uniform">
                 <div class="6u 12u$(xsmall)">
-                  <input type="text" name="name" id="name" value="" placeholder="Name" required >
+                  <input type="text" name="name" id="name" value="" placeholder="Name" required>
                 </div>
                 <div class="6u$ 12u$(xsmall)">
-                  <input type="email" name="email" id="email" value="" placeholder="Email" required >
+                  <input type="email" name="email" id="email" value="" placeholder="Email" required>
                 </div>
                 <div class="6u 12u$(xsmall)">
                   <input type="password" name="password" id="password" value="" placeholder="Password" required>
@@ -38,7 +53,7 @@ class SignupPage extends PageManager{
 
                 <div class="12u$">
                   <ul class="actions">
-                    <li><button type="submit" class="button special fit">Sign In</button></li>
+                    <li><button type="submit" class="button special fit">Signup</button></li>
                   </ul>
                 </div>
               </div>

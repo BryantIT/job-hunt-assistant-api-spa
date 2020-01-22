b515cca1c5ac51603f228a8b030d260d49c4451c
Bryant Richards <dragonseyeimages@gmail.com>
2020-01-21 19:39:22 -0600

    check token

--- client/src/adapters/decorators/signupAdapter.js
+++ client/src/adapters/decorators/signupAdapter.js
@@ -21,6 +21,7 @@ class SignupAdapter{
     })
     this.baseAdapter.checkStatus(res)
     this.baseAdapter.token = res.headers.get('authorization').split(' ')[1]
+    console.log(this.baseAdapter.token)
 
   }
 }
